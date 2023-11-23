8b4e72386763ac5038934601dddc210be351807b
simanye-pav <simathedev@gmail.com>
2023-11-23 15:51:57 +0200

    changes to build

--- src/app/components/Products/index.jsx
+++ src/app/components/Products/index.jsx
@@ -5,8 +5,47 @@ import { recordDetails } from './data';
 import Image from 'next/image';
 import { useInView } from 'react-intersection-observer';
 
-// Import statements...
+// Record component
+function Record({ record }) {
+  const hoverEffect = {
+    hover: {
+      scale: 1.05,
+      transition: { duration: 0.3 },
+    },
+  };
 
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
+  return (
+    <motion.div
+      className={`${styles.records} flex flex-col items-center justify-center`}
+      ref={ref}
+      initial="hidden"
+      animate={inView ? 'visible' : 'hidden'}
+      variants={hoverEffect}
+      whileHover="hover"
+    >
+      <motion.div
+        className="w-full"
+        variants={{ hidden: { opacity: 0, scale: 0 }, visible: { opacity: 1, scale: 1 } }}
+      >
+        <Image src={record.image} alt={record.name} width={250} height={250} className="object-cover w-full" />
+      </motion.div>
+      <motion.div
+        className="my-4 xs:h-[60px] sm:h-[60px] md:h-[80px] lg:h-[80px] xl:h-[80px] w-full text-center"
+        variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+      >
+        <p className="text-lg font-semibold">{record.name}</p>
+        <p className="text-sm">{record.artist}</p>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+// Index component
 function Index() {
   const stagger = {
     hidden: { opacity: 0, scale: 0 },
@@ -23,13 +62,6 @@ function Index() {
   const fadeInVariant = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6, delay: 0.2, ease: 'easeInOut' } },
-  };
-
-  const hoverEffect = {
-    hover: {
-      scale: 1.05,
-      transition: { duration: 0.3 },
-    },
   };
 
   const { ref, inView } = useInView({
@@ -51,9 +83,7 @@ function Index() {
   return (
     <motion.div ref={ref} initial="hidden" animate={inView ? "visible" : "hidden"} variants={fadeInVariant} className={styles.products}>
       <h1>Products</h1>
-      <p className={`${styles.description} w-[60%] py-[3rem] xs:text-[12px] sm:text-[14px] md:text-[16px] lg:text-[16px] xl:text-[16px]`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nam illum impedit nihil delectus obcaecati natus architecto fugit earum eveniet harum, sequi cumque dolore blanditiis.
-      </p>
+      {/* ... */}
       <div className={`${styles.vinyl} w-full sm:w-3/4 md:w-full lg:w-3/4 xl:w-3/4`}>
         {Object.entries(groupedRecords).map(([category, records]) => (
           <div key={category} className={styles.category}>
@@ -64,38 +94,9 @@ function Index() {
               animate="visible"
               variants={{ visible: stagger }}
             >
-              {records.map((record) => {
-                const [ref, inView] = useInView({
-                  triggerOnce: true,
-                  threshold: 0.2,
-                });
-
-                return (
-                  <motion.div
-                    key={record.id}
-                    className={`${styles.records} flex flex-col items-center justify-center`}
-                    ref={ref}
-                    initial="hidden"
-                    animate={inView ? 'visible' : 'hidden'}
-                    variants={hoverEffect}
-                    whileHover="hover"
-                  >
-                    <motion.div
-                      className="w-full"
-                      variants={{ hidden: { opacity: 0, scale: 0 }, visible: { opacity: 1, scale: 1 } }}
-                    >
-                      <Image src={record.image} alt={record.name} width={250} height={250} className="object-cover w-full" />
-                    </motion.div>
-                    <motion.div
-                      className="my-4 xs:h-[60px] sm:h-[60px] md:h-[80px] lg:h-[80px] xl:h-[80px] w-full text-center"
-                      variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
-                    >
-                      <p className="text-lg font-semibold">{record.name}</p>
-                      <p className="text-sm">{record.artist}</p>
-                    </motion.div>
-                  </motion.div>
-                );
-              })}
+              {records.map((record) => (
+                <Record key={record.id} record={record} />
+              ))}
             </motion.div>
           </div>
         ))}
--- src/app/components/Team/index.jsx
+++ src/app/components/Team/index.jsx
@@ -6,6 +6,54 @@ import { FaItunesNote } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
+function Member({ member, setHoveredMember, hoveredMember }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <motion.div
+      key={member.id}
+      className={`${styles.teamMember} xs:w-[85%] sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row shadow-xl`}
+      initial='hidden'
+      animate={inView ? 'visible' : 'hidden'}
+      variants={{
+        hidden: { opacity: 0, scale: 0 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: { delay: 0, duration: 0.9, ease: [0.43, 0.13, 0.23, 0.96] },
+        },
+      }}
+      onMouseEnter={() => setHoveredMember(member.id)}
+      onMouseLeave={() => setHoveredMember(null)}
+      ref={ref}
+    >
+      <div className={styles.imageContainer}>
+        <Image
+          src={member.image}
+          alt={member.name}
+          width={250}
+          height={250}
+          className={`${styles.teamMemberImage} ${hoveredMember === member.id ? styles.rotateImage : ''}`}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <p className={`${styles.memberOccupation} font-bold xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]}`}>{member.occupation}</p>
+        <p className={`${styles.memberName} text-[2.7rem] mt-4`}>{member.name}</p>
+        <p className={`${styles.memberDescription} py-6 xs:text-[16px] sm:text-[16px] md:text-[18px] lg:text-[22px] xl:text-[22px]`}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit nisi vel enim suscipit! Sed nesciunt explicabo voluptates reiciendis consequuntur quidem.
+        </p>
+        <FaItunesNote />
+        <p className={`${styles.memberTraits} xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]`}>{member.favGenres}</p>
+        <TbVinyl />
+        <p className={`${styles.memberTraits} xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]`}>{member.favAlbum}</p>
+      </div>
+    </motion.div>
+  );
+}
+
 function Index() {
   const [hoveredMember, setHoveredMember] = useState(null);
 
@@ -36,16 +84,6 @@ function Index() {
     },
   ];
 
-  const fadeInVariant = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6, delay: 0.2, ease: 'easeInOut' } },
-  };
-
-  const { ref: commonRef, inView: commonInView } = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
   return (
     <div className={styles.main}>
       <h1 className='xs:text-[2rem] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] xl:text-[4rem]'>Meet The Team</h1>
@@ -53,53 +91,14 @@ function Index() {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, perspiciatis.
       </p>
       <div className={`${styles.teamMembers} gap-2 xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row`}>
-        {teamMembers.map((member, index) => {
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-            threshold: 0.1,
-          });
-
-          return (
-            <motion.div
-              key={member.id}
-              className={`${styles.teamMember} xs:w-[85%] sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[90%] xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row shadow-xl`}
-              initial='hidden'
-              animate={inView ? 'visible' : 'hidden'}
-              variants={{
-                hidden: { opacity: 0, scale: 0 },
-                visible: {
-                  opacity: 1,
-                  scale: 1,
-                  transition: { delay: 0, duration: 0.9, ease: [0.43, 0.13, 0.23, 0.96] },
-                },
-              }}
-              onMouseEnter={() => setHoveredMember(member.id)}
-              onMouseLeave={() => setHoveredMember(null)}
-              ref={ref || commonRef}
-            >
-              <div className={styles.imageContainer}>
-                <Image
-                  src={member.image}
-                  alt={member.name}
-                  width={250}
-                  height={250}
-                  className={`${styles.teamMemberImage} ${hoveredMember === member.id ? styles.rotateImage : ''}`}
-                />
-              </div>
-              <div className={styles.textContainer}>
-                <p className={`${styles.memberOccupation} font-bold xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]}`}>{member.occupation}</p>
-                <p className={`${styles.memberName} text-[2.7rem] mt-4`}>{member.name}</p>
-                <p className={`${styles.memberDescription} py-6 xs:text-[16px] sm:text-[16px] md:text-[18px] lg:text-[22px] xl:text-[22px]`}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit nisi vel enim suscipit! Sed nesciunt explicabo voluptates reiciendis consequuntur quidem.
-                </p>
-                <FaItunesNote />
-                <p className={`${styles.memberTraits} xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]`}>{member.favGenres}</p>
-                <TbVinyl />
-                <p className={`${styles.memberTraits} xs:text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px]`}>{member.favAlbum}</p>
-              </div>
-            </motion.div>
-          );
-        })}
+        {teamMembers.map((member, index) => (
+          <Member
+            key={member.id}
+            member={member}
+            setHoveredMember={setHoveredMember}
+            hoveredMember={hoveredMember}
+          />
+        ))}
       </div>
     </div>
   );
